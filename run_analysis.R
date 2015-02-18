e6adc43d1e152fad523824cccbee1c214c413e11
MSena <campsensi@gmail.com>
2015-02-18 14:23:57 +0000

    R script and result
    provides final script and resulting tidy data.frame

--- run_analysis.R
+++ run_analysis.R
@@ -21,46 +21,60 @@ run_Analysis <- function(){
       names(activityLabels) <- c("activityCode", "activity")
       
       ## construct complete test data.frame (testData)
-      testData <- cbind(subjectTest,yTest,xTest); rm(subjectTest,yTest,xTest)
+      testData <- cbind(subjectTest,yTest,xTest)            #; rm(subjectTest,yTest,xTest)
             
       ## construct complete train data.frame (trainData)
-      trainData <- cbind(subjectTrain,yTrain,xTrain); rm(subjectTrain,yTrain,xTrain)
+      trainData <- cbind(subjectTrain,yTrain,xTrain)        #; rm(subjectTrain,yTrain,xTrain)
            
       ## Combine both datasets in unique dataset (fullData)
-      fullData <- rbind(testData,trainData); rm(testData,trainData)
+      fullData <- rbind(testData,trainData)                 #; rm(testData,trainData)
       
       ## Extracts only the measurements on the mean and standard deviation for 
-      ## "Triaxial acceleration from the accelerometer measurements" (AccellerometerDataMeanStd)
-      fullDataMeanStd <- fullData[,1:8]; rm(fullData)
+      ## "Triaxial acceleration from the accelerometer measurements" (AccellMeanStd)
+      accellMeanStd <- fullData[,1:8]
       
       ## Extracts only the measurements on the mean and standard deviation for 
-      ## "Triaxial Angular velocity from the gyroscope" (gyroscopeDataMeanStd)
-      ; rm(fullData)
+      ## "Triaxial Angular velocity from the gyroscope" (gyroMeanStd)
+      gyroMeanStd <- fullData[,123:128]                     #; rm(fullData)
+      
+      ## merge both sensors data
+      
+     meanStd <- cbind(accellMeanStd,gyroMeanStd)
       
       ## Using descriptive activity names to name the activities in the data set (while rearranging the columns order too)
       library(dplyr)
           
       dataSet <- activityLabels %>% 
-            merge(fullDataMeanStd, by.x = "activityCode", by.y = "V1.1") %>% 
+            merge(meanStd, by.x = "activityCode", by.y = "V1.1") %>% 
             arrange(V1, activityCode) %>%  
-            select (-activityCode) %>% 
-            select(V1, activity, V1.2, V2, V3, V4, V5, V6); rm(fullDataMeanStd, activityLabels)
+            select (-activityCode)                          #; rm(meanStd, activityLabels)
       
       ## Appropriately labels the data set with descriptive variable names
-            names(dataSet)[1] <- "subject"
-            dataSet$subject <- as.factor(dataSet$subject)
-            varNames <- as.character(features$V2[1:6])
-            names(dataSet)[3:8] <- varNames ;rm(varNames, features)
+            names(dataSet)[2] <- "subject"
+            dataSet$subject   <- as.factor(dataSet$subject)
+            
             
       
       ## From the dataSet, creates a ftable with 
       ## the average of each variable for each activity and each subject.
       library(reshape2)
-            dataMelt <- melt(dataSet, id = c("subject","activity"), measure.vars = 3:8)
-            secondTable <- dataMelt %>% acast( subject ~ activity ~ variable, mean) %>% ftable ; rm(dataMelt)
-            
-      ##Write secondTable to a file      
-      write.ftable(secondTable,"./secondTable.txt",row.name = FALSE)
+            dataMelt    <- melt(dataSet, id = c("subject","activity"), measure.vars = 3:14)      #; rm(dataSet)
+            fTable      <- dataMelt %>% acast( subject ~ activity ~ variable, mean) %>% ftable  #; rm(dataMelt)
+            data        <- data.frame(expand.grid(rev(attr(fTable, "row.vars"))), unclass(fTable))
       
-      secondTable
+      ## Appropriately labels the data set with descriptive variable names
+      names(data)[1:2]  <- c("activity","subject")
+     
+      varNames <- as.character(features$V2[1:6])
+      names(data)[3:8]  <- varNames                         #; rm(features)  
+     
+      varNames <- as.character(features$V2[121:126])
+      names(data)[9:14] <- varNames                         #; rm(varNames)
+     
+     finalTable <- select(data, subject, activity, 3:14)
+     
+      ##Write secondTable to a file      
+      write.table(finalTable,"./finalTable.txt",row.name = FALSE)
+      
+      finalTable
 }
