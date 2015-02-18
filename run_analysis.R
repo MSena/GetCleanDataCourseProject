c2ce63d04409da30796b932c9894760db4a043b0
MSena <campsensi@gmail.com>
2015-02-18 14:53:22 +0000

    Run_analysis.R script
    reviewed R script

--- run_analysis.R
+++ run_analysis.R
@@ -17,17 +17,17 @@ run_Analysis <- function(){
       yTrain            <- read.table("./UCI _HAR_Dataset/train/y_train.txt")
       xTrain            <- read.table("./UCI _HAR_Dataset/train/X_train.txt")
       
-      ## prior work: name activityLabels variables
+      ## Prior work: name activityLabels variables
       names(activityLabels) <- c("activityCode", "activity")
       
-      ## construct complete test data.frame (testData)
-      testData <- cbind(subjectTest,yTest,xTest)            #; rm(subjectTest,yTest,xTest)
+      ## Construct a complete test data.frame (testData)
+      testData <- cbind(subjectTest,yTest,xTest)            ; rm(subjectTest,yTest,xTest)
             
-      ## construct complete train data.frame (trainData)
-      trainData <- cbind(subjectTrain,yTrain,xTrain)        #; rm(subjectTrain,yTrain,xTrain)
+      ## Construct a complete train data.frame (trainData)
+      trainData <- cbind(subjectTrain,yTrain,xTrain)        ; rm(subjectTrain,yTrain,xTrain)
            
       ## Combine both datasets in unique dataset (fullData)
-      fullData <- rbind(testData,trainData)                 #; rm(testData,trainData)
+      fullData <- rbind(testData,trainData)                 ; rm(testData,trainData)
       
       ## Extracts only the measurements on the mean and standard deviation for 
       ## "Triaxial acceleration from the accelerometer measurements" (AccellMeanStd)
@@ -35,46 +35,50 @@ run_Analysis <- function(){
       
       ## Extracts only the measurements on the mean and standard deviation for 
       ## "Triaxial Angular velocity from the gyroscope" (gyroMeanStd)
-      gyroMeanStd <- fullData[,123:128]                     #; rm(fullData)
+      gyroMeanStd <- fullData[,123:128]                     ; rm(fullData)
       
-      ## merge both sensors data
+      ## merge both sensors (accelerometer and gyroscope) data
+      meanStd <- cbind(accellMeanStd,gyroMeanStd)
       
-     meanStd <- cbind(accellMeanStd,gyroMeanStd)
+      ## Using descriptive "activity names" to name the activities in the data set 
       
-      ## Using descriptive activity names to name the activities in the data set (while rearranging the columns order too)
       library(dplyr)
           
       dataSet <- activityLabels %>% 
             merge(meanStd, by.x = "activityCode", by.y = "V1.1") %>% 
-            arrange(V1, activityCode) %>%  
-            select (-activityCode)                          #; rm(meanStd, activityLabels)
+                  arrange(V1, activityCode) %>%  
+                        select (-activityCode)              ; rm(meanStd, activityLabels)
       
-      ## Appropriately labels the data set with descriptive variable names
+      ## Appropriately label the data set with descriptive variable names
             names(dataSet)[2] <- "subject"
             dataSet$subject   <- as.factor(dataSet$subject)
             
             
       
-      ## From the dataSet, creates a ftable with 
-      ## the average of each variable for each activity and each subject.
+      ## From dataSet, create a ftable with the average value of each variable,
+      ## for each activity-subject pair, and return a data.frame
       library(reshape2)
             dataMelt    <- melt(dataSet, id = c("subject","activity"), measure.vars = 3:14)      #; rm(dataSet)
             fTable      <- dataMelt %>% acast( subject ~ activity ~ variable, mean) %>% ftable  #; rm(dataMelt)
             data        <- data.frame(expand.grid(rev(attr(fTable, "row.vars"))), unclass(fTable))
       
-      ## Appropriately labels the data set with descriptive variable names
+      ## Appropriately label the final data set with descriptive variable names
       names(data)[1:2]  <- c("activity","subject")
      
       varNames <- as.character(features$V2[1:6])
-      names(data)[3:8]  <- varNames                         #; rm(features)  
+      names(data)[3:8]  <- varNames                         ; rm(features)  
      
       varNames <- as.character(features$V2[121:126])
-      names(data)[9:14] <- varNames                         #; rm(varNames)
+      names(data)[9:14] <- varNames                         ; rm(varNames)
      
      finalTable <- select(data, subject, activity, 3:14)
      
-      ##Write secondTable to a file      
+      ## Write finalTable to a file      
       write.table(finalTable,"./finalTable.txt",row.name = FALSE)
       
-      finalTable
+     ## Presents table on Rstudio Console
+      View(finalTable)                                      ; rm(finalTable)
+     
+     ## farewell statement 
+      print("finalTable with requested result written to 'finalTable.txt' on courseProject Directory.")
 }
